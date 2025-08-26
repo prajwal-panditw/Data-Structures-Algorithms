bdc1ea51d1b3578b543ce78cb4cf05a6446c2962
Prajwal Pandit <prajwalpandit.w@gmail.com>
2025-08-26 20:08:46 +0530

    Basic Recursion added.

--- STL/.vscode/queue.cpp
+++ STL/.vscode/queue.cpp
@@ -2,25 +2,36 @@
 using namespace std;
 int main()
 {
-  queue<int>q,q1;
-  cout<<q.size()<<endl;
-  q.push(1);
-  cout<<q.back()<<" "<<q.front()<<endl;
-  q.emplace(2);
-  cout<<q.back()<<" "<<q.front()<<endl;
-  q.push(3);
-  q.push(4);
-  cout<<q.back()<<" "<<q.front()<<endl;
-  cout<<q.size()<<endl;
-  q.pop();
-  cout<<q.back()<<" "<<q.front()<<endl;
-  cout<<q.size()<<endl;
-  q.pop();
-  cout<<q.back()<<" "<<q.front()<<endl;
-  q1.push(100);
-  q1.push(50);
-  q1.swap(q);
-  cout<<q.back()<<" "<<q.front()<<endl;
+  // queue<int>q,q1;
+  // cout<<q.size()<<endl;
+  // q.push(1);
+  // cout<<q.back()<<" "<<q.front()<<endl;
+  // q.emplace(2);
+  // cout<<q.back()<<" "<<q.front()<<endl;
+  // q.push(3);
+  // q.push(4);
+  // cout<<q.back()<<" "<<q.front()<<endl;
+  // cout<<q.size()<<endl;
+  // q.pop();
+  // cout<<q.back()<<" "<<q.front()<<endl;
+  // cout<<q.size()<<endl;
+  // q.pop();
+  // cout<<q.back()<<" "<<q.front()<<endl;
+  // q1.push(100);
+  // q1.push(50);
+  // q1.swap(q);
+  // cout<<q.back()<<" "<<q.front()<<endl;
+  queue<int>qu;
+  for(int i=1;i<=5;i++)
+  {
+    qu.push(i);
+  }
+  // cout<<qu.front()<<" "<<qu.back();
+  while(!qu.empty())
+  {
+    cout<<qu.front()<<"\n";
+    qu.pop();
+  }
 
 
 
